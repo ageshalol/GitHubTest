cba872f65bec95b35d6ec87dc487e25dc481712e
ageshalol <vges147@gmail.com>
2023-07-06 18:30:11 +0600

    lolC:/Users/99677/AppData/Local/Programs/Python/Python38/python.exe

---
+++ test.py
@@ -0,0 +1,10 @@
+import random
+
+def generate_passworld():
+    letters = "qwerzcmns,cnkask.cl;amsc"
+    result + ""
+    for i in range(8):
+        random_letter = random.choice(letters)
+        result += random_letter
+    print(result)
+generate_passworld()
